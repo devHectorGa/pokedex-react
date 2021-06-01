0fd6304fc59409866ddc9cd1cf060b2ccb4c6f68
devHectorGa <contacto@devHector.Ga>
2021-06-01 16:56:05 -0500

    Get data from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from 'react';
+const BASE_PATH = process.env.REACT_APP_BASE_PATH;
+
 function App() {
-  return <div className="App"></div>;
+  const [pokemonList, setPokemonList] = useState(null);
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      await fetch(`${BASE_PATH}pokemon?limit=20`)
+        .then((response) => response.json())
+        .then((result) => setPokemonList(result));
+    })();
+  }, []);
+
+  return <main className="App">Pokemon</main>;
 }
 
 export default App;
