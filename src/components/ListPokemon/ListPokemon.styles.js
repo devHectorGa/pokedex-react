9c0c3221085056ff72caf83cc6243760ba8f0710
devHectorGa <contacto@devHector.Ga>
2021-06-01 17:18:26 -0500

    print Initial Data

--- src/components/ListPokemon/ListPokemon.styles.js
+++ src/components/ListPokemon/ListPokemon.styles.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(auto, 15rem));
+  align-items: center;
+  justify-content: center;
+  margin: 2rem auto;
+  gap: 1rem;
+  width: 90%;
+`;
--- src/components/Pokemon/Pokemon.styles.js
+++ src/components/Pokemon/Pokemon.styles.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const Container = styled.article``;
+export const Container = styled.article`
+  background-color: var(--color2);
+  padding: 1rem;
+  border-radius: 0.5rem;
+`;
